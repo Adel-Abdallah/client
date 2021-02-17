292b7871fde281e76c4df0f5a69407062d3f4645
Adel-Abdallah <adelgafo@gmail.com>
2021-02-17 05:16:15 +0200

    https://github.com/Adel-Abdallah/shopping-list.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppNavBar from "./components/AppNavBar";
+import ItemModal from "./components/ItemModal";
+import ShoppingList from "./components/ShoppingList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from "react-redux";
+import store from "./store";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className='App'>
+          <AppNavBar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
